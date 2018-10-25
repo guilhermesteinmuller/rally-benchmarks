fe5b787618cc85a0147b0cb1ff518feb33ec5442
Guilherme Steinmuller <gsteinmuller@vexxhost.com>
2018-10-25 18:21:23 +0100

    remove verify after generate reort

--- rally-tempest.sh
+++ rally-tempest.sh
@@ -2,13 +2,14 @@
 
 SCENARIOS=( compute network image identity volume )
 report_file_name="rally-$(date +"%d-%b-%y")-tempest"
-tempest_version="rally verify list-verifiers | grep $id | awk '{ print $16 }' | sed -e 's/\Status\>//g' | sed '/^$/d'"
+tempest_version="rally verify list-verifiers | grep $1 | awk '{ print $16 }' | sed -e 's/\Status\>//g' | sed '/^$/d'"
 
 for i in "${SCENARIOS[@]}" 
 do
     rally verify start --id $1  --pattern set=$i
     verify_id=rally verify list | awk '{print $2}' | sed -e 's/\UUID\>//g' | sed '/^$/d'
-    rally verify report --uuid $(verify_id) --type html --to "$({report_file_name}_${tempest_version}_${i}).html" 
+    rally verify report --uuid $(verify_id) --type html --to "$({report_file_name}_${tempest_version}_${i}).html"
+    rally veriry delete --uuid $(verify_id) 
 done
 
 
