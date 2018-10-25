9b66d79e57e8f4030e5b17d3d4767c9f93594609
Guilherme Steinmuller <gsteinmuller@vexxhost.com>
2018-10-25 18:19:55 +0100

    add ability to run tempest per pattern and generate report

--- rally-tempest.sh
+++ rally-tempest.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 SCENARIOS=( compute network image identity volume )
-report_file_name="rally-$(date +"%d-%b-%y")"
+report_file_name="rally-$(date +"%d-%b-%y")-tempest"
+tempest_version="rally verify list-verifiers | grep $id | awk '{ print $16 }' | sed -e 's/\Status\>//g' | sed '/^$/d'"
 
 for i in "${SCENARIOS[@]}" 
 do
-    echo "rally verify start --id $1  --pattern set=$i"
+    rally verify start --id $1  --pattern set=$i
+    verify_id=rally verify list | awk '{print $2}' | sed -e 's/\UUID\>//g' | sed '/^$/d'
+    rally verify report --uuid $(verify_id) --type html --to "$({report_file_name}_${tempest_version}_${i}).html" 
 done
 
 
